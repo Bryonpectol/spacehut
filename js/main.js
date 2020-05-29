c379b1642653da3748bad522e11cacd13676df46
Bryonpectol <46723917+Bryonpectol@users.noreply.github.com>
2020-05-28 23:17:51 -0600

    Testing js

---
+++ js/main.js
@@ -0,0 +1,11 @@
+let NavMobileElement = document.getElementById("navmobile");
+let ImgIconMobile = document.getElementById("imgicon-mobile");
+let NavMobileUl = document.getElementById("nav-mobile-ul");
+let NavMobileLi = document.getElementById("nav-mobile-li");
+let NavMobileA = document.getElementById("nav-mobile-a");
+
+NavMobileElement = false;
+ImgIconMobile = false;
+NavMobileUl = false;
+NavMobileLi = false;
+NavMobileA = false;
