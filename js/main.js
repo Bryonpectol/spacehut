ad01482f177e5d6b815e7c40328aa04e43d81c5d
Bryonpectol <46723917+Bryonpectol@users.noreply.github.com>
2020-05-28 23:30:45 -0600

    Change li name

--- js/main.js
+++ js/main.js
@@ -1,8 +1,15 @@
 let NavMobileElement = document.getElementById("navmobile");
 let ImgIconMobile = document.getElementById("imgicon-mobile");
 let NavMobileUl = document.getElementById("nav-mobile-ul");
-let NavMobileLi = document.getElementById("nav-mobile-li");
-let NavMobileA = document.getElementById("nav-mobile-a");
+let NavMobileLi1 = document.getElementById("nav-mobile-li-one");
+let NavMobileLi2 = document.getElementById("nav-mobile-li-two");
+let NavMobileLi3 = document.getElementById("nav-mobile-li-three");
+let NavMobileLi4 = document.getElementById("nav-mobile-li-four");
+
+let NavMobileA1 = document.getElementById("nav-mobile-a-one");
+let NavMobileA2 = document.getElementById("nav-mobile-a-two");
+let NavMobileA3 = document.getElementById("nav-mobile-a-three");
+let NavMobileA4 = document.getElementById("nav-mobile-a-four");
 
 NavMobileElement = false;
 ImgIconMobile = false;
