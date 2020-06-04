e873cb873a8f2a2b5516f21696fd1f789b9b8a93
Bryon <bryon.pectol@gmail.com>
2020-06-03 20:33:34 -0600

    added hamburger on index

--- js/main.js
+++ js/main.js
@@ -1,37 +1,10 @@
-function MyFunction () {
+function MyFunction() {
+    var x = document.getElementById("navmobile");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
 
-let NavMobileElement = document.getElementById("navmobile");
-let ImgIconMobile = document.getElementById("imgicon-mobile");
-let NavMobileUl = document.getElementById("nav-mobile-ul");
-let NavMobileLi1 = document.getElementById("nav-mobile-li-one");
-let NavMobileLi2 = document.getElementById("nav-mobile-li-two");
-let NavMobileLi3 = document.getElementById("nav-mobile-li-three");
-let NavMobileLi4 = document.getElementById("nav-mobile-li-four");
 
-let NavMobileA1 = document.getElementById("nav-mobile-a-one");
-let NavMobileA2 = document.getElementById("nav-mobile-a-two");
-let NavMobileA3 = document.getElementById("nav-mobile-a-three");
-let NavMobileA4 = document.getElementById("nav-mobile-a-four");
-
-let menuBtn = document.createElement("button");
-
-menuBtn = false;
-
-if(menuBtn = false) {
-
-NavMobileElement.ClassName = "none";
-ImgIconMobile.ClassName = "none";
-NavMobileUl.ClassName = "none";
-NavMobileLi1.ClassName = "none";
-NavMobileLi2.ClassName = "none";
-NavMobileLi3.ClassName = "none";
-NavMobileLi4.ClassName = "none";
-NavMobileA1.ClassName = "none";
-NavMobileA2.ClassName = "none";
-NavMobileA3.ClassName = "none";
-NavMobileA4.ClassName = "none";
-
- }
-
-document.body.appendChild(menuBtn);
-}
