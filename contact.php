88a51356d3ec034f251c588c2627b61d1261c8f0
Bryon <bryon.pectol@gmail.com>
2020-06-17 21:09:04 -0600

    testing GET

--- contact.php
+++ contact.php
@@ -1,14 +1,14 @@
 <?php
 
-    if(isset($_POST['firstname'])) {
-        $FirstName = $_POST['firstname'];
+    if(isset($_GET['firstname'])) {
+        $FirstName = $_GET['firstname'];
         $handle = fopen('names.txt', 'a');
         fwrite($handle, $FirstName."\n");
         fwrite($handle, $LastName."\n");
         fclose($handle);
     }
 
-    $LastName = $_POST['lastname'];
+    $LastName = $_GET['lastname'];
 
     echo $FirstName;
 ?>
